2503564c1a590d632b566af4f802f590a03ecf47
lpoznanski <113032553+lpoznanski@users.noreply.github.com>
2025-08-26 11:41:27 +0200

    register after pb-link is defined

--- pb-folio.js
+++ pb-folio.js
@@ -1,25 +1,29 @@
-export class PbFolio extends PbLink {
-  static get properties() {
-    return {
-      ...super.properties,
-      xmlId2: { type: String, attribute: 'xml-id2' },
-      channel2: { type: String, attribute: 'channel2' }
-    };
-  }
+customElements.whenDefined('pb-link').then(() => {
+  const PbLink = customElements.get('pb-link');
 
-  constructor() {
-    super();
-    this.xmlId2 = null;
-    this.channel2 = 'ask3';
-  }
+  class PbFolio extends PbLink {
+    static get properties() {
+      return {
+        ...super.properties,
+        xmlId2:   { type: String, attribute: 'xml-id2' },
+        channel2: { type: String, attribute: 'channel2' }
+      };
+    }
 
-  _onClick(ev) {
-    ev.preventDefault();
-    super._onClick(ev);
-    if (this.xmlId2) {
-      this.emitTo(this.channel2, { id: this.xmlId2 });
+    constructor() {
+      super();
+      this.xmlId2   = null;
+      this.channel2 = 'ask3';
+    }
+
+    _onClick(ev) {
+      ev.preventDefault();
+      super._onClick(ev); // klasyczne pb-link emit="navi" xml-id="…"
+      if (this.xmlId2) {
+        this.emitTo(this.channel2, { id: this.xmlId2 }); // drugi strzał na channel2
+      }
     }
   }
-}
 
-customElements.define('pb-folio', PbFolio);
+  customElements.define('pb-folio', PbFolio);
+});
